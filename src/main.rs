e2eca0ecd4b3705642dc707898594a32ed5c9a82
Samuel Araújo <samuel.aracor@gmail.com>
2026-02-24 01:40:08 -0300

    feat: altera estrutura dos módulos e immplementa demo da montagem de acordes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-mod modules;
-use crate::modules::common::*;
+mod instrument;
+mod render;
+mod theory;
+
+use crate::theory::chords::*;
 
 fn main() {
-    let note = "1233";
-    let scale_prev = get_scale(note, &MAJOR_SCALE_INTERVALS);
-    println!("Resultado: {:?}", scale_prev);
-    let my_key = validate_key("J#");
-    // let my_key = if validate_key("C#") { "válido" } else { "inválido" };
-    println!("Seu tom é: {:?}", my_key)
+  let pattern = "F#5maj7";
+  let (extentions, remaining) = strip_extensions(pattern);
+
+  println!("Extensions: {:?}\n remaining: {:?}", extentions, remaining);
 }
